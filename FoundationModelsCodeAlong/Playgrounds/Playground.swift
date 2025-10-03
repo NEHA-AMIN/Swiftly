27741c8603c5139303fcd915383c925eee2e2892
Neha Amin <145868361+NEHA-AMIN@users.noreply.github.com>
2025-10-03 05:57:42 +0530

    Initial Commit

---
+++ FoundationModelsCodeAlong/Playgrounds/Playground.swift
@@ -0,0 +1,42 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A Playground for testing Foundation Models framework features.
+*/
+
+import FoundationModels
+import Playgrounds
+#Playground {
+    let instructions = """
+        Your job is to create an itinerary for the user.
+        Each day needs an activity, hotel and restaurant.
+
+        Always include a title, a short description, and a day-by-day plan.
+        """
+
+    let session = LanguageModelSession(instructions: instructions)
+    let response = try await session.respond(to: "Generate a 3-day itinerary to Paris.")
+}
+#Playground {
+    let model = SystemLanguageModel.default
+
+    // The availability property provides detailed information on the model's state.
+    switch model.availability {
+        case .available:
+            print("Foundation Models is available and ready to go!")
+
+        case .unavailable(.deviceNotEligible):
+            print("The model is not available on this device since device is not eligible")
+
+        case .unavailable(.appleIntelligenceNotEnabled):
+            print("Apple Intelligence is not enabled in your Settings.")
+
+        case .unavailable(.modelNotReady):
+            print("The model is not ready yet. Please try again later this is a temporary issue .")
+
+        case .unavailable(let other):
+            print("The model is unavailable for an unknown reason.")
+    }
+}
+
