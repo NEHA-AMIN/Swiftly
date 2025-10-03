f5594d31f2819ad14986c77e11b5de040a3a794e
Neha Amin <145868361+NEHA-AMIN@users.noreply.github.com>
2025-10-03 06:02:47 +0530

    first commit

---
+++ .gitignore.swift
@@ -0,0 +1,17 @@
+//
+//  .gitignore.swift
+//  FoundationModelsCodeAlong
+//
+//  Created by Neha  on 03/10/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+// macOS
+// .DS_Store
+
+// Xcode
+// DerivedData/
+// build/
+// xcuserdata/
+// *.xcuserstate
+
